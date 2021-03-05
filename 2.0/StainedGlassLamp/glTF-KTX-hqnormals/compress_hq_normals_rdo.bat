0d0bd3b145536a53ed70f35300425fa1498c162e
Richard Sahlin <richard_sahlin@yahoo.com>
2021-03-05 13:20:46 +0100

    Add KTX2 files for high quality compression
    Compression uses a combination of UASTC and ETCS1.
    UASTC compressed textures are treated in two ways -
    one normal, for most linear textures, and one
    high quality for normal map textures.
    The model will have very small visible differences
    when compared to the source model:

---
+++ 2.0/StainedGlassLamp/glTF-KTX-hqnormals/compress_hq_normals_rdo.bat
@@ -0,0 +1,24 @@
+set mipmap=--genmipmap
+set uastc=--uastc 4 --uastc_rdo_q 4 --zcmp 20
+set uastchq=--uastc 4 --uastc_rdo_q 0.05 --uastc_rdo_d 65536 --zcmp 20
+set etcs1=--bcmp --clevel 4 --qlevel 255
+set folder=
+toktx %mipmap% %etcs1%  ..\%folder%\StainedGlassLamp_base_basecolor.ktx2 StainedGlassLamp_base_basecolor.png
+toktx %mipmap% %etcs1%  ..\%folder%\StainedGlassLamp_base_emissive.ktx2 StainedGlassLamp_base_emissive.png
+toktx %mipmap% %uastc% ..\%folder%\StainedGlassLamp_glass_basecolor-alpha.ktx2 StainedGlassLamp_glass_basecolor-alpha.png
+toktx %mipmap% %etcs1%  ..\%folder%\StainedGlassLamp_glass_emissive.ktx2 StainedGlassLamp_glass_emissive.png
+toktx %mipmap% %etcs1%  ..\%folder%\StainedGlassLamp_grill_basecolor-alpha.ktx2 StainedGlassLamp_grill_basecolor-alpha.png
+toktx %mipmap% %etcs1%  ..\%folder%\StainedGlassLamp_grill_emissive.ktx2 StainedGlassLamp_grill_emissive.png
+toktx %mipmap% %etcs1%  ..\%folder%\StainedGlassLamp_hardware_basecolor.ktx2 StainedGlassLamp_hardware_basecolor.png
+toktx %mipmap% %etcs1%  ..\%folder%\StainedGlassLamp_hardware_emissive.ktx2 StainedGlassLamp_hardware_emissive.png
+
+toktx %mipmap% %uastchq% --linear  ..\%folder%\StainedGlassLamp_base_normal.ktx2 StainedGlassLamp_base_normal.png
+toktx %mipmap% %uastchq% --linear  ..\%folder%\StainedGlassLamp_base_occlusion-rough-metal.ktx2 StainedGlassLamp_base_occlusion-rough-metal.png
+toktx %mipmap% %uastc% --linear  ..\%folder%\StainedGlassLamp_bulbs_occlusion-rough-metal.ktx2 StainedGlassLamp_bulbs_occlusion-rough-metal.png
+toktx %mipmap% %uastchq% --linear  ..\%folder%\StainedGlassLamp_glass_normal.ktx2 StainedGlassLamp_glass_normal.png
+toktx %mipmap% %uastc% --linear  ..\%folder%\StainedGlassLamp_glass_occlusion-rough-metal.ktx2 StainedGlassLamp_glass_occlusion-rough-metal.png
+toktx %mipmap% %uastchq% --linear  ..\%folder%\StainedGlassLamp_grill_normal.ktx2 StainedGlassLamp_grill_normal.png
+toktx %mipmap% %uastc% --linear  ..\%folder%\StainedGlassLamp_grill_occlusion-rough-metal.ktx2 StainedGlassLamp_grill_occlusion-rough-metal.png
+toktx %mipmap% %uastchq% --linear  ..\%folder%\StainedGlassLamp_hardware_normal.ktx2 StainedGlassLamp_hardware_normal.png
+toktx %mipmap% %uastc% --linear  ..\%folder%\StainedGlassLamp_hardware_occlusion-rough-metal.ktx2 StainedGlassLamp_hardware_occlusion-rough-metal.png
+toktx %mipmap% %uastc% --linear  ..\%folder%\StainedGlassLamp_steel_occlusion-rough-metal.ktx2 StainedGlassLamp_steel_occlusion-rough-metal.png
